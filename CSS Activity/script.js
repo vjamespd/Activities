1521c5d7c51a4aeee4fe5a05d902887309b6c7c7
vjamespd <dvinardjames@gmail.com>
2024-10-25 22:16:25 +0800

    added an embedded youtube video

---
+++ CSS Activity/script.js
@@ -0,0 +1,11 @@
+// Show popup
+function showPopup() {
+    document.getElementById("videoPopup").style.display = "block";
+    document.querySelector(".overlay").style.display = "block";
+}
+
+// Hide popup
+function hidePopup() {
+    document.getElementById("videoPopup").style.display = "none";
+    document.querySelector(".overlay").style.display = "none";
+}
