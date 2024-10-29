29ba0d847b990420b69b5d5be9642c7517d0fc13
vjamespd <dvinardjames@gmail.com>
2024-10-30 00:38:57 +0800

    added a popup content

--- CSS Activity/script.js
+++ CSS Activity/script.js
@@ -9,3 +9,8 @@ function hidePopup() {
     document.getElementById("videoPopup").style.display = "none";
     document.querySelector(".overlay").style.display = "none";
 }
+function togglePopup() { 
+    const popup = document.querySelector('.popup'); 
+    if (popup.style.bottom === '0px') { popup.style.bottom = '-100%'; }
+    else { popup.style.bottom = '0'; }
+}
